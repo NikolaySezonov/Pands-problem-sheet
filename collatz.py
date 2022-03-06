19b91ca77d30763d295bdcb350c39578e7cea32d
NikolaySezonov <nikolay.sezonov@gmail.com>
2022-03-06 17:08:40 +0000

    lecture 4

---
+++ collatz.py
@@ -0,0 +1,26 @@
+# program that asks the user to input any positive integer and outputs the successive values of the following calculation.
+# At each step calculate the next value by taking the current value and, 
+#   if it is even, divide it by two, but if it is odd, multiply it by three and add one.
+# Have the program end if the current value is one.
+
+# Author Nikolay Sezonov
+
+from itertools import count
+
+
+def collatz(number):
+
+    if number % 2 == 0:
+
+        print(number // 2)
+        return number // 2
+
+    elif number % 2 == 1:
+     
+        result = 3 * number + 1
+        print(result)
+        return result
+
+n = input("Please enter a positive integer : ")
+while n != 1:
+    n = collatz(int(n))
