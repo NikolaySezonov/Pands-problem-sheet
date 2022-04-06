ecd88e3ac4a0cbf82b2fefbf4f247b6b2744b114
NikolaySezonov <nikolay.sezonov@gmail.com>
2022-04-06 21:30:24 +0100

    Problem sheets 5,6,7,8 final

--- es.py
+++ es.py
@@ -3,18 +3,39 @@
 # Reference http://book.pythontips.com/en/latest/open_function.html
 # Refernence:https://cmdlinetips.com/2011/08/three-ways-to-read-a-text-file-line-by-line-in-python/
 
+total = 0
+line_nr = 0
+counter = 0
+
+# variables required for total number and letter count
+letr = input ('Please enter the letter you want to count:')
+# this prompt user to enter the required letter to count
+
 with open('moby-dick.txt', 'r') as text_file:
 # this opens the text document from the command line and 'r' imports the readline function
 
-    count = 0
-    # The count is set at zero for the for loop
-
     for line in text_file:
+        line_nr += 1
+        # add 1 to every line in the loop
+
+        print ('Number of letters in Line No - ',line_nr)
+        for letter in line:
+        # It will look for the specific letter in the line. 
+        # It will add 1 every time it found the letter until the counted line is ended
+            if letter == letr:
+                counter += 1
+                total +=1
+                # two counts run simulanuesly 
+        print (counter)
+        counter = 0
+    print ('Total number of required letter:',(total))
+    
+
     # the for loop goes through each line in the text file
     # The count is looking at the line and adding 1 every time it runs through the for loop.
-        count == 0
-        count+=1    
+        
+        
         # If the count is even and has no remainders then the if statement is true.
-
-               # If the if statement is true it will print the line.
-        print(line.count("e"))
+        
+               # If the if statement is true it will count number of "e" in each line, but i cannot get the total :(
+        
